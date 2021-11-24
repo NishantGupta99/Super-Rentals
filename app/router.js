cf2dd85a227ce277eb4db0800fa0eb91f5bb9e51
nishant Gupta <kr.nish99@gmail.com>
2021-11-24 17:49:41 +0530

    creation of another page and basic routing

--- app/router.js
+++ app/router.js
@@ -6,4 +6,6 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('about')
+}); // this will be used to route pages
